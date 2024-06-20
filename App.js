82bbb7dd0a931c1a66cb00bceb64f382e76e761f
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2024-06-20 23:25:55 +0300

    add web support for screenshot

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useRef, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import DomToImage from 'dom-to-image';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -58,19 +59,37 @@ export default function App() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Image saved!');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Image saved!');
+        }
+      } catch (e) {
+        alert('An error has occured.');
+        console.log(e);
       }
-    } catch (e) {
-      alert('An error has occured.');
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await DomToImage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        alert('An error has occured.');
+        console.log(e);
+      }
     }
   };
 
